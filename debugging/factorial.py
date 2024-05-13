5522b71a482711e3c91a8bcf8d8e37d87f593907
Yasine31130 <yassine.benslika@holbertonstudents.com>
2024-05-13 03:12:44 -0700

    0. Debugging - Python Factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import sys
 
-
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n = n - 1
     return result
 
-
-f = factorial(int(sys.argv[1]))
-print(f)
+if len(sys.argv) > 1:  # Check if an argument is provided
+    try:
+        input_number = int(sys.argv[1])  # Ensure the input can be converted to an integer
+        f = factorial(input_number)
+        print(f)
+    except ValueError:  # Handle the case where the input is not an integer
+        print("Please provide an integer number as an argument.")
+else:
+    print("Please provide a number as an argument.")
